4d0725851cb15703f6a4ea831ddf9d48f2641e2f
JhaPrashant2102 <jhaprashant2102@gmail.com>
2020-09-30 23:01:38 +0530

    UC3 all test cases added

--- junitdemo/moodanalyser/src/main/java/com/moodanalyser/MoodAnalyser.java
+++ junitdemo/moodanalyser/src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -12,13 +12,18 @@ public class MoodAnalyser {
 		this.message = null;
 	}
 
+	//Used for UC2
+	/*
 	public String analyseMood(String message) {
+		
 		if (message.contains("sad"))
 			return "SAD";
 		else
 			return "HAPPY";
 	}
 
+	*/
+	/*
 	public String analyseMood() {
 		try {
 			if(message.contains("sad"))
@@ -29,5 +34,40 @@ public class MoodAnalyser {
 			return "HAPPY";
 		}
 	}
-
+	*/
+	
+	
+	
+	//for UC3TC1 custom exception is being used to throw exception
+	
+	public String analyseMood(String message) throws MoodAnalysisException {
+		this.message = message;
+		return analyseMood();
+	}
+	
+	public String analyseMood() throws MoodAnalysisException {
+		try {
+			if(message.contains("sad")) 
+			return "SAD";
+			else return "HAPPY";
+		}
+		catch(NullPointerException e) {
+			throw new MoodAnalysisException("Please Enter Some Message !");
+		}
+	} 
+	//for UC3TC2
+	/*
+	 public String analyseMood() throws MoodAnalysisException {
+		try {
+			if(message.length()==0)
+				throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,"Please Enter Some Message !");
+			if(message.contains("sad")) 
+			return "SAD";
+			else return "HAPPY";
+		}
+		catch(NullPointerException e) {
+			throw new MoodAnalysisException("Please Enter Some Message !");
+		}
+	}
+	 */
 }
